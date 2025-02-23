e5616a53a555c75baaf160f75bbb81bb7d202667
khaerulwalid <ckrwislam@gmail.com>
2025-02-23 22:24:33 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyApp {
+    class Program {
+        static void Main(string[] args) {
+            Person person1 = new Person("John", 25);
+            Person person2 = new Person("John", 25, "USA");
+
+            person1.DisplayInfo();
+            person2.DisplayInfo();
+        }
+    }
+
+    class Person {
+        public string Name {
+            get; private set;
+        }
+        private int Age {
+            get; set;
+        }
+        private string Address {
+            get; set;
+        }
+        public Person(string name, int age) {
+            this.Name = name;
+            this.Age = age;
+            this.Address = "Unknow";
+        }
+
+        public Person(string name, int age, string address) : this(name, age) {
+            this.Address = address;
+        }
+
+        public void DisplayInfo() {
+            Console.WriteLine($"Name: {Name}, Age: {Age}, Address: {Address}");
+        }
+    }
+}
